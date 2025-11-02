4148481247d8e98ba749fa3612019f6ffcc9c037
sharath <sharathshetty301@gmail.com>
2025-11-02 18:37:10 +0530

    fix: update API base URL to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: [
+//     "http://localhost:5000/api",
+//     "https://sweet-shop-management-system-t7w1.onrender.com/api",
+//   ], // your backend
+//   withCredentials: true,
+// });
+
+// export default api;
+
 const api = axios.create({
-  baseURL: [
-    "http://localhost:5000/api",
-    "https://sweet-shop-management-system-t7w1.onrender.com/api",
-  ], // your backend
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
 export default api;
